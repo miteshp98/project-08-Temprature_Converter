4160eaf369551ba83f37369633805c316e387380
Mitesh Panchal <miteshpanchal458@gmail.com>
2024-05-21 15:58:51 +0530

    Add if input null show errror

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const convertOption = document.querySelector("#convert-opt");
 const converBtn = document.querySelector(".submit-btn");
 const resultElem = document.querySelector(".result");
 
-inputNumber.value = 0;
-
 converBtn.addEventListener("click", function (e) {
   e.preventDefault();
   tempratureConverter();
 });
 
 function tempratureConverter() {
-  if (tempratureOption.value === "C" && convertOption.value === "f") {
+  if (inputNumber.value === "") {
+    resultElem.textContent = null;
+    document.querySelector(".error").classList.add("visible");
+  } else if (tempratureOption.value === "C" && convertOption.value === "f") {
+    document.querySelector(".error").classList.remove("visible");
     resultElem.innerHTML = `${
       (inputNumber.value * 9) / 5 + 32
     } <sup> &degF</sup>`;
